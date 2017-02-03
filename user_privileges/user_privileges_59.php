b39bfd72afa4fca40a7088f95c81c2541c0dbf45
Jonathan <informatica@tuagencia24.com>
2017-02-03 18:01:25 -0430

    filtro loc por satelite y boletos

--- user_privileges/user_privileges_59.php
+++ user_privileges/user_privileges_59.php
@@ -4,9 +4,9 @@
 //This is the access privilege file
 $is_admin=false;
 
-$current_user_roles='H15';
+$current_user_roles='H14';
 
-$current_user_parent_role_seq='H1::H2::H3::H4::H5::H15';
+$current_user_parent_role_seq='H1::H2::H3::H4::H5::H14';
 
 $current_user_profiles=array(14,);
 
@@ -24,5 +24,5 @@ $parent_roles=array('H1','H2','H3','H4','H5',);
 
 $subordinate_roles_users=array();
 
-$user_info=array('user_name'=>'bestravel','is_admin'=>'off','user_password'=>'$1$be000000$DeMJjXmMOGIEjNWy1wHa//','confirm_password'=>'$1$be000000$DeMJjXmMOGIEjNWy1wHa//','first_name'=>'Bestravel','last_name'=>'Satelite','roleid'=>'H15','email1'=>'bestravelcoro@gmail.com','status'=>'Active','activity_view'=>'Today','lead_view'=>'Today','hour_format'=>'12','end_hour'=>'','start_hour'=>'00:00','title'=>'','phone_work'=>'0268-2510831','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'dd-mm-yyyy','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'3hGMoHhVmygEJnIV','time_zone'=>'UTC','currency_id'=>'2','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'0','theme'=>'softed','language'=>'es_es','reminder_interval'=>'30 Minutes','asterisk_extension'=>'','use_asterisk'=>'0','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'0','dayoftheweek'=>'Monday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'1','rowheight'=>'medium','cedula'=>'J-40635332-9','firma'=>'','ccurrency_name'=>'','currency_code'=>'VEF','currency_symbol'=>'Bs','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','currency_name'=>'Venezuela, Bolivares Fuertes','id'=>'59');
+$user_info=array('user_name'=>'bestravel','is_admin'=>'off','user_password'=>'$1$be000000$DeMJjXmMOGIEjNWy1wHa//','confirm_password'=>'$1$be000000$DeMJjXmMOGIEjNWy1wHa//','first_name'=>'Bestravel','last_name'=>'Satelite','roleid'=>'H14','email1'=>'bestravelcoro@gmail.com','status'=>'Active','activity_view'=>'Today','lead_view'=>'Today','hour_format'=>'12','end_hour'=>'','start_hour'=>'00:00','title'=>'','phone_work'=>'0268-2510831','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'dd-mm-yyyy','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'3hGMoHhVmygEJnIV','time_zone'=>'UTC','currency_id'=>'2','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'0','theme'=>'softed','language'=>'es_es','reminder_interval'=>'30 Minutes','asterisk_extension'=>'','use_asterisk'=>'0','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'0','dayoftheweek'=>'Monday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'1','rowheight'=>'medium','cedula'=>'J-40635332-9','firma'=>'BLAJ0641W |##| ','accountid'=>'13020','ccurrency_name'=>'','currency_code'=>'VEF','currency_symbol'=>'Bs','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','currency_name'=>'Venezuela, Bolivares Fuertes','id'=>'59');
 ?>
